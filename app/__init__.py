8a79feb5357e9bec5104442d6ad569012bbc6e37
Rishit <ui21cs51@iiitsurat.ac.in>
2026-01-29 11:49:40 +0530

    main branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from .extensions import mongo
+from .webhook.routes import webhook
+import os
+import configparser
+config = configparser.ConfigParser()
+config.read("app/sample.ini")
 
-from app.webhook.routes import webhook
-
-
+# Setup MongoDB here
 # Creating our flask app
 def create_app():
 
     app = Flask(__name__)
-    
     # registering all the blueprints
     app.register_blueprint(webhook)
-    
+    mongo.init_app(app,uri=config['TEST']['DB_URI'])
     return app
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,73 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, render_template, request
+from datetime import datetime
+from app.extensions import mongo
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
+
+
+
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
+    payload = request.get_json(force=True)
+    event = request.headers.get("X-GitHub-Event")
+    events = mongo.db.events  
+    doc = None
+
+    if event == "push":
+        author = payload["pusher"]["name"]
+        to_branch = payload["ref"].split("/")[-1]
+        after = payload['after']
+        doc = {
+            "request_id":after,
+            "action": "PUSH",
+            "author": author,
+            "to_branch": to_branch,
+            "timestamp": datetime.utcnow()
+        }
+
+    elif event == "pull_request":
+        action = payload["action"]
+
+        author = payload["pull_request"]["user"]["login"]
+        from_branch = payload["pull_request"]["head"]["ref"]
+        to_branch = payload["pull_request"]["base"]["ref"]
+        pr_id = str(payload["pull_request"]["number"])
+        if action == "closed" and payload["pull_request"]["merged"]:
+            doc = {
+                "request_id":pr_id,
+                "action": "MERGE",
+                "author": author,
+                "from_branch": from_branch,
+                "to_branch": to_branch,
+                "timestamp": datetime.utcnow()
+            }
+
+        elif action == "opened":
+            doc = {
+                "request_id":pr_id,
+                "action": "PULL_REQUEST",
+                "author": author,
+                "from_branch": from_branch,
+                "to_branch": to_branch,
+                "timestamp": datetime.utcnow()
+            }
+
+    if doc:
+        events.insert_one(doc)
+        print("Saved:", doc)
+
     return {}, 200
+
+@webhook.route('/events', methods=['GET'])
+def get_events():
+    events = mongo.db.events
+    data = list(events.find({}, {"_id": 0}).sort("timestamp", -1).limit(20))
+    return data, 200
+@webhook.route('/ui', methods=['GET'])
+def ui():
+    return render_template("index.html")
+
+@webhook.route('/', methods=['GET'])
+def ping():
+    return {"message": "Everything is good"}, 200
